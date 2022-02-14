5698b3e79f7b32e7132d82c7adbcbbed32a22ff1
imedfactonics <imedsaoud@factonics.com>
2022-02-14 15:37:26 +0100

    empty commit

--- src/config/config.js
+++ src/config/config.js
@@ -8,11 +8,6 @@ const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
     PORT: Joi.number().default(3000),
-    DB_HOST: Joi.string().description('host to connect to the maria db'),
-    DB_PORT: Joi.number().description('port to connect to the maria db'),
-    DB_USER: Joi.string().description('user of the db'),
-    DB_PWD: Joi.string().description('pwd of the db'),
-    DB_NAME: Joi.string().description('name of the db'),
   })
   .unknown();
 
@@ -26,10 +21,10 @@ module.exports = {
   env: envVars.NODE_ENV,
   port: envVars.PORT,
   mariadb: {
-    host: envVars.DB_HOST,
-    port : envVars.DB_PORT,
-    user : envVars.DB_USER,  
-    pwd : envVars.DB_PWD,
-    name : envVars.DB_NAME,      
+    host: "163.172.130.142",
+    port : 3310,
+    user : 'etudiant',  
+    pwd : 'CrERP29qwMNvcbnAMgLzW9CwuTC5eJHn',
+    name : 'sakila',      
   }
 };

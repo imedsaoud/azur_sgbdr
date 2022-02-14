f39fa38c37e1ca9bfa3293a18e83201e8013dc6a
imedfactonics <imedsaoud@factonics.com>
2022-02-14 15:25:46 +0100

    deployment config V0.1

--- src/app.js
+++ src/app.js
@@ -47,9 +47,6 @@ app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
 
-// convert error to ApiError, if needed
-app.use(errorConverter);
-
 // handle error
 app.use(errorHandler);
 
